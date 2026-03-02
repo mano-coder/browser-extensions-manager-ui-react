30927b5e2a62d42c6d10b2bc0eba88dd8ca9e1a4
mano-coder <bis.anouar5@gmail.com>
2026-03-02 03:27:01 +0000

    feat: display extensions from data.json

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,19 @@
+import { useState, useEffect } from "react";
 import Header from "./components/Header";
 import ExtensionSection from "./components/ExtensionSection";
-import FilterSection from "./components/FilterSection"
-import Footer from "./components/Footer"
+import FilterSection from "./components/FilterSection";
+import Footer from "./components/Footer";
+import data from "./data/data.json";
 
 export default function App() {
+  const [extensionList, setExtensionList] = useState(data);
+
   return (
     <>
       <Header />
       <main>
         <FilterSection />
-        <ExtensionSection />
+        <ExtensionSection extensionList={extensionList} />
       </main>
       <Footer />
     </>
--- src/components/ExtensionSection.jsx
+++ src/components/ExtensionSection.jsx
@@ -1,3 +1,35 @@
-export default function ExtensionSection() {
-  return (<section id="extensions-section"></section>)
+export default function ExtensionSection({ extensionList }) {
+  const extensionMap = extensionList.map(
+    ({ logo, name, description, isActive }) => {
+      return (
+        <div className="card" data-name="${name}" key={name}>
+          <div className="card-header">
+            <div className="logo-wrapper">
+              <img src={logo} alt={`${name} Logo`} />
+            </div>
+            <div className="content">
+              <h3>{name}</h3>
+              <p>{description}</p>
+            </div>
+          </div>
+          <div className="card-footer">
+            <button className="remove-btn">Remove</button>
+            <div className="toggle-switch">
+              <label className="slider-container">
+                <input
+                  type="checkbox"
+                  className={`sr-only `}
+                  checked={isActive}
+                  onChange={() => {}}
+                  aria-label={`Toggle status for ${name}`}
+                />
+                <span className="slider"></span>
+              </label>
+            </div>
+          </div>
+        </div>
+      );
+    },
+  );
+  return <section id="extensions-section">{extensionMap}</section>;
 }
