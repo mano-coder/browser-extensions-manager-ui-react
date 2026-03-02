636c0e5eaa9d21f5292f241fcebfaac8eb5894d5
mano-coder <bis.anouar5@gmail.com>
2026-03-02 04:04:53 +0000

    feat: theme switch

--- src/App.jsx
+++ src/App.jsx
@@ -8,9 +8,24 @@ import data from "./data/data.json";
 export default function App() {
   const [extensionList, setExtensionList] = useState(data);
 
+  const [theme, setTheme] = useState(localStorage.getItem("theme") || "light");
+
+  useEffect(() => {
+    localStorage.setItem("theme", theme);
+    if (theme === "dark") {
+      document.body.classList.add("dark-theme");
+    } else {
+      document.body.classList.remove("dark-theme");
+    }
+  }, [theme]);
+
+  const handleClick = () => {
+    return setTheme((prevState) => (prevState === "dark" ? "light" : "dark"));
+  };
+
   return (
     <>
-      <Header />
+      <Header handleClick={handleClick} />
       <main>
         <FilterSection />
         <ExtensionSection extensionList={extensionList} />
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -3,13 +3,13 @@ import darkLogo from "../assets/images/dark-logo.svg";
 import iconSun from "../assets/images/icon-sun.svg";
 import iconMoon from "../assets/images/icon-moon.svg";
 
-export default function Header() {
+export default function Header({ handleClick }) {
   return (
     <header>
       <nav>
         <img src={logo} alt="dark-logo" className="logo light-logo" />
         <img src={darkLogo} alt="logo" className="logo dark-logo" />
-        <button type="button" id="theme-switch-btn">
+        <button type="button" id="theme-switch-btn" onClick={() => handleClick()}>
           <img className="icon-moon" src={iconMoon} alt="moon" />
           <img className="icon-sun" src={iconSun} alt="sun" />
         </button>
