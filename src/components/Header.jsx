5f96c77ae09f7fc12a2fe29eabe0741363b85619
mano-coder <bis.anouar5@gmail.com>
2026-03-02 05:13:47 +0000

    fix: change handleClick function name to handleThemeToggle

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -3,13 +3,17 @@ import darkLogo from "../assets/images/dark-logo.svg";
 import iconSun from "../assets/images/icon-sun.svg";
 import iconMoon from "../assets/images/icon-moon.svg";
 
-export default function Header({ handleClick }) {
+export default function Header({ handleThemeToggle }) {
   return (
     <header>
       <nav>
         <img src={logo} alt="dark-logo" className="logo light-logo" />
         <img src={darkLogo} alt="logo" className="logo dark-logo" />
-        <button type="button" id="theme-switch-btn" onClick={() => handleClick()}>
+        <button
+          type="button"
+          id="theme-switch-btn"
+          onClick={() => handleThemeToggle()}
+        >
           <img className="icon-moon" src={iconMoon} alt="moon" />
           <img className="icon-sun" src={iconSun} alt="sun" />
         </button>
